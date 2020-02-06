5e01669a3bdfaae6fa76ac10e3006508fee6a9b6
Eric Huang <56049709+HE-1234@users.noreply.github.com>
2020-02-06 13:52:39 -0800

    Add files via upload

--- FractionDriver.java
+++ FractionDriver.java
@@ -9,29 +9,23 @@ public class FractionDriver {
         Fraction f3 = new Fraction("3/4");
         Fraction f4 = new Fraction(f2);
 
-
         System.out.println(f1);
         System.out.println(f2);
         System.out.println(f3);
         System.out.println(f4);
 
-
         System.out.println("\nTesting arithmetic methods...\n");
-
 
         System.out.println( f3 + " + " + f4 + " = " + Fraction.add(f3, f4) );
         System.out.println( f3 + " - " + f4 + " = " + Fraction.subtract(f3, f4) );
         System.out.println( f3 + " x " + f4 + " = " + Fraction.multiply(f3, f4) );
         System.out.println( f3 + " ÷ " + f4 + " = " + Fraction.divide(f3, f4) );
 
-
         System.out.println("\nTesting denominator zero fails...\n");
-
 
         Fraction f5 = new Fraction(1,0);
         Fraction f6 = new Fraction("1/0");
         Fraction f7 = new Fraction("0/1");
-
 
         System.out.println( f4 + " ÷ " + f7 + " = " + Fraction.divide(f4, f7) );
         
